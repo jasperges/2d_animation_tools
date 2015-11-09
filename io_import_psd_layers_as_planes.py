ae5644e0155476b99339459cfbd16f92b66f27da
jasperge <jaspervannieuwenhuizen@gmail.com>
2015-11-09 22:54:49 +0100

    Change location of group empties based on their immediate children (Closes #111)
    Added in a bit 'clumsy' way. It would be a lot nicer to read the location of the children from the layer_info and create the empties at the correct location in the first place. :)

--- io_import_psd_layers_as_planes.py
+++ io_import_psd_layers_as_planes.py
@@ -26,6 +26,7 @@ import random
 import string
 import psd_tools
 import bpy
+import mathutils
 from bpy.props import (BoolProperty,
                        StringProperty,
                        FloatProperty,
@@ -38,6 +39,34 @@ def generate_random_id(length=8):
                      string.ascii_lowercase,
                      string.ascii_uppercase))
     return ''.join(random.choice(chars) for _ in range(length))
+
+
+def pivot_to_children(obj):
+
+    def sum_vectors(vectors):
+        vector_sum = mathutils.Vector()
+        for v in vectors:
+            vector_sum += v
+        return vector_sum
+
+    def select_objects(objects):
+        for obj in objects:
+            obj.select = True
+
+    children = obj.children
+    child_world_mats = [child.matrix_world.copy() for child in children]
+    child_locations = [mat.translation for mat in child_world_mats]
+    child_median = sum_vectors(child_locations) / len(children)
+    new_location = obj.matrix_world.inverted() * child_median + obj.location
+    bpy.ops.object.select_all(action='DESELECT')
+    select_objects(children)
+    bpy.context.scene.objects.active = children[0]
+    bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
+    obj.location = new_location
+    bpy.ops.object.select_all(action='DESELECT')
+    select_objects(children)
+    bpy.context.scene.objects.active = obj
+    bpy.ops.object.parent_set()
 
 
 def parse_psd(self, psd_file):
@@ -128,9 +157,14 @@ def create_objects(self, layer_info, image_size, img_dir, psd_name, layers, impo
     '''
 
     def get_parent(parent_name, import_id):
-        for obj in bpy.data.objects:
+        for obj in bpy.context.scene.objects:
             if parent_name in obj.name and obj['import_id'] == import_id:
                 return obj
+
+    def get_all_parent_empties(import_id):
+        for obj in bpy.context.scene.objects:
+            if obj.type == 'EMPTY' and obj['import_id'] == import_id:
+                yield obj
 
     def group_object(obj, parent, root_group, group_empty, group_group, import_id):
         if group_empty:
@@ -237,6 +271,10 @@ def create_objects(self, layer_info, image_size, img_dir, psd_name, layers, impo
             i += 1
 
     if group_empty:
+        parent_empties = get_all_parent_empties(import_id)
+        for empty in parent_empties:
+            if not empty == root_empty:
+                pivot_to_children(empty)
         bpy.ops.object.select_all(action='DESELECT')
         root_empty.select = True
         bpy.context.scene.objects.active = root_empty
